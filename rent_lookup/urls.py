9ced235f49439435e11e96a90dc27fa0def6de9c
Hallux <grosnourson@msn.com>
2016-10-26 23:41:12 +0200

    reaorder for accueil and rent_lookup fixed

--- rent_lookup/urls.py
+++ rent_lookup/urls.py
@@ -6,8 +6,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<city_id>[0-9]+)/(?P<residence_id>[0-9]+)/$', views.details, name='details'),
-    url(r'^(?P<city_id>[0-9]+)/$', views.residence, name='residence'),
+    url(r'^(?P<residence_id>[0-9]+)/$', views.details, name='details'),
     url(r'^$', views.city, name='city'),
-    #url(r'^$', views.globalize, name='globalize'),
 ]
--- rent_lookup/views.py
+++ rent_lookup/views.py
@@ -3,33 +3,14 @@ from django.shortcuts import render
 # from .models import Residence, City, Department, Ecole, Logement, Categorie
 from .models import Residence, City, Categorie
 
-class ResidenceBag(object):
-
-    residence = None
-    ecoles = []
-    logements = []
-    images = []
-
 def city(request):
     cities = list(City.objects.all().order_by("city_name"))
-    #cities = City.objects.filter(departement=department_id).order_by("city_name")
     categories = Categorie.objects.all()
     return render(request,'rent_lookup/department.html', {'cities': cities, 'categories': categories})
 
 
-def residence(request, city_id):
-    residences = Residence.objects.filter(city=city_id).order_by("residence_name")
-    categories = Categorie.objects.all()
-    return render(request,'rent_lookup/city.html', {'residences': residences, 'categories': categories})
-
-
-def details(request, city_id, residence_id):
+def details(request, residence_id):
     residence = Residence.objects.get(pk=residence_id)
     categories = Categorie.objects.all()
     return render(request,'rent_lookup/details.html',{'residence': residence, 'categories': categories})
 
-
-#def globalize(request):
-#    all_department = list(Department.objects.all().order_by("departement_name"))
-#    categories = Categorie.objects.all()
-#    return render(request,'rent_lookup/globalize.html',{'all_department': all_department, 'categories': categories})
