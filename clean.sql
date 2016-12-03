b5640776675c8fa72a22d047917768f4c4ef24ae
root <root@vps348316.ovh.net>
2016-12-03 22:18:36 +0100

    last push

---
+++ clean.sql
@@ -0,0 +1,7 @@
+use lecheng_bis;
+delete from rent_lookup_ecole where id>1;
+delete from rent_lookup_logement where id>1;
+delete from rent_lookup_image where id>1;
+delete from rent_lookup_residence where id>1;
+delete from rent_lookup_city where id>1;
+
